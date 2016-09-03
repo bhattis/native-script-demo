61016b56d6684df70dee1f7deb4a99865b98e1ad
Sandeep Bhatti <sandeep@metawarelabs.com>
2016-09-03 21:29:23 +0530

    few changes

--- app/model/user.ts
+++ app/model/user.ts
@@ -1,19 +1,17 @@
 const validator = require("email-validator");
 
-// @Injectable()
 export class User {
   
-  first_name: string;
-  last_name: string;
+  name: string;
   email: string;
   password: string;
-  gender: string;
+  userToken: string;
+  objectId: string;
+
+  constructor() { }
   
   isValidEmail() {
     return validator.validate(this.email);
   }
 
-  fullname() {
-    return `#{this.first_name} #{this.last_name}`
-  }
 }
